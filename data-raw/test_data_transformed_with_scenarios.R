82f405978b079f7c570003f2d0893b3fa981609f
Elliott <elliott.messeiller@gmail.com>
2022-01-20 16:09:38 +0200

    Adding definition of test_data_transformed_with_scenarios

---
+++ data-raw/test_data_transformed_with_scenarios.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+test_data_transformed_with_scenarios <- load_misc_data("test_data/test_data/test_data.parquet") %>%
+  billionaiRe::make_default_scenario() %>%
+  dplyr::filter(!stringr::str_detect(.data[["ind"]], "espar|detect_respond")) %>%
+  billionaiRe::add_scenario("accelerate") %>%
+  billionaiRe::add_scenario("halt_rise") %>%
+  billionaiRe::transform_hep_data(scenario = "scenario") %>%
+  billionaiRe::transform_hpop_data() %>%
+  billionaiRe::transform_uhc_data()
+
+arrow::write_parquet(test_data_transformed_with_scenarios, "data-raw/test_data_transformed_with_scenarios.parquet")
+
+whdh::upload_to_data_lake(billionaiRe::get_data_lake_name(),
+                          source_path = "data-raw/test_data_transformed_with_scenarios.parquet",
+                          destination_path = "3B/Bronze/misc/test_data/test_data_transformed_with_scenarios/test_data_transformed_with_scenarios.parquet",
+                          add_timestamp = T)
